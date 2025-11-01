690e7288d7e4bb4d597ee146a847faaaf283eb73
Liudmyla Hetmanenko <162147950+Liudmyla11@users.noreply.github.com>
2025-11-01 18:25:32 +0200

    Update ProgramCodeModified.java

--- ProgramCodeModified.java
+++ ProgramCodeModified.java
@@ -1,9 +1,17 @@
-package com.example;
-
+/**
+ * Головний клас програми, що виводить назву продукту
+ */
 public final class ProgramCodeModified {
 
     private ProgramCodeModified() {
+        // Приватний конструктор
     }
+
+    /**
+     * Головний метод програми
+     *
+     * @param args Аргументи командного рядка
+     */
     public static void main(final String[] args) {
         System.out.println("Meal-Sing-Connect-Finance");
     }
